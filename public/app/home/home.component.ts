88531eb7f27317dad3b8570b7653d990ec8e44a4
Adan <twinadanz@gmail.com>
2018-06-08 13:59:57 -0500

    add test onInit commands for home.component

--- public/app/home/home.component.ts
+++ public/app/home/home.component.ts
@@ -1,10 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import * as io from 'socket.io-client';
 
 @Component({
     moduleId: module.id,
+    selector: 'ng-home'
     templateUrl: 'home.template.html'
 })
 
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+    socket: SocketIOClient.Socket;
+
+    constructor() {
+        this.socket = io.connect();
+    }
+
+    ngOnInit() {
+        this.socket.emit('event1', {
+            msg: 'Client to server, can you hear me server?'
+        });
+
+        this.socket.on('event2', (data:any) => {
+            console.log(data.msg, " << msg from event2");
+            this.socket.emit('event3', {
+                msg: "Yes, all systems GO!"
+            })
+        })
+
+        this.socket.on('event4', (data:any) => {
+            console.log(data.msg, " << msg from event4");
+        })
+    }
 
 }

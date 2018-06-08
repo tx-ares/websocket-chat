cef8c0c5a34b29e29d74d24b84881e9faef0f813
Adan <twinadanz@gmail.com>
2018-06-08 12:54:47 -0500

    create server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+const port = 8000; // Setting server port to 8000
+
+app.use(express.static(path.join(__dirname."public")));
+
+io.on('connection', (socket) => {
+    console.log('** Connection Established **');
+    
+})

e96bcd2c8a3257cfc9ffbf399f3e324dc20d10cf
Adan <twinadanz@gmail.com>
2018-06-08 14:00:49 -0500

    add server test commands

--- server.js
+++ server.js
@@ -6,11 +6,25 @@ const io = require('socket.io')(server);
 
 const port = 8000; // Setting server port to 8000
 
-app.use(express.static(path.join(__dirname."public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 io.on('connection', (socket) => { //
     console.log('** Connection Established **');
 
+    socket.on('event1', (data) => { // Message from client
+        console.log(data.msg , " << message event1")
+    });
+
+    socket.emit('event2', { // When a message is sent to socket server, the server emits an event and console logs a msg.
+        msg: 'Server to client, do you read me? Over.'
+    });
+
+    socket.on('event3', (data) => {
+        console.log(data.msg, " << message event3");
+        socket.emit('event4', {
+            msg: 'Client to Server: Loud and clear! :D'
+        })
+    })
 })
 
 server.listen(port, () => { // Callback for
